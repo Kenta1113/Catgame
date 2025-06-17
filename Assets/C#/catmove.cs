77b5aa660557dfbf0de3bce06420ac71b666d8ae
kenta17291113 <kenta17291113@gmail.com>
2025-06-17 16:25:58 +0900

    攻撃の時に画像が切り替わる処理

--- Assets/C#/catmove.cs
+++ Assets/C#/catmove.cs
@@ -1,52 +1,78 @@
+using System.Collections;
 using UnityEngine;
 
 public class CatMove : MonoBehaviour
 {
     [SerializeField] float _speed = 3f;
     [SerializeField] float _jump = 5f;
+    [SerializeField] float _scratchDuration = 0.3f; // スクラッチ表示時間
+    [SerializeField] private Sprite attackSprite;    // 攻撃時のスプライト
 
     private Animator anim;
     private Rigidbody2D rb;
-
+    private SpriteRenderer sr;
+    private Sprite defaultSprite;                    // 元のスプライト
     private bool isGrounded = false;
+    private bool isScratching = false;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        defaultSprite = sr.sprite; // 現在のスプライトを記録
     }
 
     void Update()
     {
         float move = Input.GetAxisRaw("Horizontal");
 
-        // 移動
-        rb.velocity = new Vector2(move * _speed, rb.velocity.y);
-
-        // 左右反転
-        if (move != 0)
+        if (!isScratching)
         {
-            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
+            // 移動
+            rb.velocity = new Vector2(move * _speed, rb.velocity.y);
+
+            // 左右反転
+            if (move != 0)
+            {
+                transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
+            }
+
+            // ジャンプ
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, _jump);
+            }
+
+            // アニメーション再生制御
+            anim.speed = (isGrounded && move != 0) ? 1f : 0f;
         }
 
-        // ジャンプ
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        // 攻撃処理
+        if (Input.GetButtonDown("Fire1") && !isScratching)
         {
-            rb.velocity = new Vector2(rb.velocity.x, _jump);
-        }
-
-        // アニメーション切り替え（地面にいるときだけ）
-        if (isGrounded)
-        {
-            anim.SetFloat("Speed", Mathf.Abs(move));
-        }
-        else
-        {
-            anim.SetFloat("Speed", 0f); // 空中ではSpeedを0に
+            StartCoroutine(DoScratch());
+            Debug.Log("攻撃");
         }
     }
 
-    // 地面に触れたとき
+    IEnumerator DoScratch()
+    {
+        isScratching = true;
+
+        // アニメーション止めてスプライト切り替え
+        anim.enabled = false;
+        sr.sprite = attackSprite;
+
+        yield return new WaitForSeconds(_scratchDuration);
+
+        // 元のスプライトとアニメーションに戻す
+        sr.sprite = defaultSprite;
+        anim.enabled = true;
+
+        isScratching = false;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -55,7 +81,6 @@ public class CatMove : MonoBehaviour
         }
     }
 
-    // 地面から離れたとき
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
