090cb413458590041fd6fc2dff960496e5ddeddb
kenta17291113 <kenta17291113@gmail.com>
2025-06-22 22:50:36 +0900

    落下死を追加

--- Assets/C#/CatController.cs
+++ Assets/C#/CatController.cs
@@ -9,9 +9,11 @@ public class CatMove : MonoBehaviour
     [SerializeField] private Sprite attackSprite;
     [SerializeField] GameObject _scratchPrefab;
     [SerializeField] Transform _claw = default;
-    [SerializeField] int _hp = 3;
+    [SerializeField] int _life = 3;
     [SerializeField] float _attackCooldown = 1f;
     [SerializeField] UIHPmanager uIHPmanager;
+
+    Transform _respawnPoint;
 
     float _lastAttackTime = -Mathf.Infinity;
     private Animator anim;
@@ -21,17 +23,24 @@ public class CatMove : MonoBehaviour
     private bool isGrounded = false;
     private bool isScratching = false;
 
-    public string _goalanime = "PlayerGoal";
-    public string _gameover = "Gameover";
-
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         defaultSprite = sr.sprite;
-        Debug.Log("初期HP: " + _hp);
-        uIHPmanager.SetHp(_hp);
+
+        GameObject startObj = GameObject.FindGameObjectWithTag("Start");
+        if (startObj != null)
+        {
+            _respawnPoint = startObj.transform;
+        }
+        else
+        {
+            Debug.LogError("Startタグがついたオブジェクトが見つかりません！");
+        }
+
+        uIHPmanager.SetHp(_life);
     }
 
     void Update()
@@ -41,20 +50,16 @@ public class CatMove : MonoBehaviour
         if (!isScratching)
         {
             rb.velocity = new Vector2(move * _speed, rb.velocity.y);
-            //移動
+
             if (move != 0)
-            {
                 transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
-            }
-            //ジャンプ
+
             if (Input.GetButtonDown("Jump") && isGrounded)
-            {
                 rb.velocity = new Vector2(rb.velocity.x, _jump);
-            }
 
             anim.speed = (isGrounded && move != 0) ? 1f : 0f;
         }
-        //攻撃
+
         if (Input.GetButtonDown("Fire1") && !isScratching && Time.time >= _lastAttackTime + _attackCooldown)
         {
             _lastAttackTime = Time.time;
@@ -71,41 +76,31 @@ public class CatMove : MonoBehaviour
             scratch.transform.localScale = scratchScale;
 
             ScratchController bc = scratch.GetComponent<ScratchController>();
-            if (bc != null)
-            {
-                bc.SetDirection(direction);
-            }
+            if (bc != null) bc.SetDirection(direction);
         }
     }
 
-    void TakeDamage()
+    void DieAndRespawn()
     {
-        _hp--;
-        uIHPmanager.SetHp(_hp);
+        _life--;
+        uIHPmanager.SetHp(_life);
 
-        if (_hp <= 0)
+        if (_life <= 0)
         {
-            Debug.Log("Gameover");
-            rb.velocity = Vector2.zero;
-            // anim.Play(_gameover); // 必要ならコメントアウト解除
+            Debug.Log("Game Over");
+            // ゲームオーバー処理
+            return;
         }
-        else
-        {
-            Debug.Log("ダメージを受けた → HP: " + _hp);
-        }
-    }
 
-    void Heal(int amount)
-    {
-        _hp += amount;
-        uIHPmanager.SetHp(_hp);
-        Debug.Log("HPを回復 → HP: " + _hp);
+        Debug.Log("残機: " + _life + " → リスポーン");
+
+        transform.position = _respawnPoint.position;
+        rb.velocity = Vector2.zero;
     }
 
     IEnumerator DoScratch()
     {
         isScratching = true;
-
         anim.enabled = false;
         sr.sprite = attackSprite;
 
@@ -113,35 +108,26 @@ public class CatMove : MonoBehaviour
 
         sr.sprite = defaultSprite;
         anim.enabled = true;
-
         isScratching = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
-        {
             isGrounded = true;
-        }
         else if (collision.gameObject.CompareTag("Enemy"))
-        {
-            TakeDamage();
-        }
+            DieAndRespawn(); // 敵に当たったらミス扱い
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
-        {
             isGrounded = false;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Item"))
-        {
-            Heal(1);
-        }
+        if (collision.gameObject.CompareTag("Fall"))
+            DieAndRespawn(); // 落下死もここで処理
     }
 }
